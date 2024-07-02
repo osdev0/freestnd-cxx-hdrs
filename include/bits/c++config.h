24481082fd61b588bd7ae7cd031d488e10827d8f
ilobilo <ilobilo@users.noreply.github.com>
2024-07-02 17:59:51 +0000

    Upload headers [ci skip]

--- include/bits/c++config.h
+++ include/bits/c++config.h
@@ -36,7 +36,7 @@
 #define _GLIBCXX_RELEASE 15
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20240607
+#define __GLIBCXX__ 20240702
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
