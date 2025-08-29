0909bda15d7bcd1067468152bc2f9fee1bc94749
Mintsuki <mintsuki@protonmail.com>
2025-08-29 05:29:37 +0200

    Update gcc to 14.3.0 and binutils to 2.45

--- make_toolchain.sh
+++ make_toolchain.sh
@@ -15,11 +15,11 @@ if [ -z "$TARGET" ]; then
 fi
 
 if [ -z "$BINUTILSVERSION" ]; then
-    BINUTILSVERSION=2.44
+    BINUTILSVERSION=2.45
 fi
 
 if [ -z "$GCCVERSION" ]; then
-    GCCVERSION=14.2.0
+    GCCVERSION=14.3.0
 fi
 
 if command -v gmake; then
@@ -55,25 +55,28 @@ export MAKEFLAGS="-j$(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || psri
 
 export PATH="$PREFIX/bin:$PATH"
 
-if [ ! -f binutils-$BINUTILSVERSION.tar.gz ]; then
-    curl -o binutils-$BINUTILSVERSION.tar.gz https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILSVERSION.tar.gz
+if [ ! -f binutils-$BINUTILSVERSION.tar.xz ]; then
+    curl -Lo binutils-$BINUTILSVERSION.tar.xz https://ftpmirror.gnu.org/gnu/binutils/binutils-$BINUTILSVERSION.tar.xz
+    b2sum binutils-$BINUTILSVERSION.tar.xz | grep -q 1ce72346b1f531c89feb86b407e2c649151b506ffbd1a02d413411d36f7ede98fa9a1adf75dd941c01df5fe7e6bf151828b269eeb7c278315ca8004bff22eb7f
 fi
-if [ ! -f gcc-$GCCVERSION.tar.gz ]; then
-    curl -o gcc-$GCCVERSION.tar.gz https://ftp.gnu.org/gnu/gcc/gcc-$GCCVERSION/gcc-$GCCVERSION.tar.gz
+if [ ! -f gcc-$GCCVERSION.tar.xz ]; then
+    curl -Lo gcc-$GCCVERSION.tar.xz https://ftpmirror.gnu.org/gnu/gcc/gcc-$GCCVERSION/gcc-$GCCVERSION.tar.xz
+    b2sum gcc-$GCCVERSION.tar.xz | grep -q 11c0e549b2e9b4bbbe4cd64782032d2ec783b3db8d4aa538ebd0a0c9760d8e521c32007891a608b081bc6dd353d4eb23030c5e2b9fe9a20c4894a8455dde47b6
 fi
 
 rm -rf build
 mkdir build
 cd build
 
-$TAR -zxf ../binutils-$BINUTILSVERSION.tar.gz
-$TAR -zxf ../gcc-$GCCVERSION.tar.gz
+$TAR -xf ../binutils-$BINUTILSVERSION.tar.xz
+$TAR -xf ../gcc-$GCCVERSION.tar.xz
 
 cd binutils-$BINUTILSVERSION
 # Apply patches, if any
 for patch in "${srcdir}"/toolchain-patches/binutils/*; do
     [ "${patch}" = "${srcdir}/toolchain-patches/binutils/*" ] && break
     patch -p1 < "${patch}"
+    find . -name '*.orig' -delete
 done
 cd ..
 mkdir build-binutils
@@ -88,11 +91,12 @@ cd gcc-$GCCVERSION
 for patch in "${srcdir}"/toolchain-patches/gcc/*; do
     [ "${patch}" = "${srcdir}/toolchain-patches/gcc/*" ] && break
     patch -p1 < "${patch}"
+    find . -name '*.orig' -delete
 done
 sed 's|http://gcc.gnu|https://gcc.gnu|g' < contrib/download_prerequisites > dp.sed
 mv dp.sed contrib/download_prerequisites
 chmod +x contrib/download_prerequisites
-./contrib/download_prerequisites --no-verify
+./contrib/download_prerequisites
 cd ..
 mkdir build-gcc
 cd build-gcc
